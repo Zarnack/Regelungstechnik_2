0748dcf4f80aeb4e3a41f162837dae1a4b2b0df3
Zarnack <Sebastian.zarnack@ivi.fraunhofer.de>
2020-07-05 00:39:36 +0200

    Neunordnung, etwas Kommentierung, DT1 Glied hinzugefügt

--- Function_Blocks.py
+++ Function_Blocks.py
@@ -3,21 +3,25 @@ import scipy.integrate as sci
 
 
 class Calculate:
-    def __init__(self, state_sv):
+    def __init__(self, activate, state_sv):
         self.state_sv = [state_sv]
+        self.activate = activate
 
     def ode(self, t, x, u):
         pass
 
     def calc(self, u, t0, t1):
-        solv = sci.solve_ivp(self.ode, (t0, t1), self.state_sv, args=(u,))
-        self.state_sv = solv.y.T[1:, 0]
-        return solv.y.T[0][0]
+        if self.activate:
+            solv = sci.solve_ivp(self.ode, (t0, t1), self.state_sv, args=(u, ))
+            self.state_sv = solv.y.T[1:, 0]
+            return solv.y.T[0][0]
+        else:
+            return 0
 
 
 class PT1_Glied(Calculate):
-    def __init__(self, Kp, T, state_sv=0):
-        super().__init__(state_sv)
+    def __init__(self, Kp, T, activate=True, state_sv=0):
+        super().__init__(activate, state_sv)
         self.Kp = Kp
         self.T = T
 
@@ -28,8 +32,8 @@ class PT1_Glied(Calculate):
 
 
 class I_Glied(Calculate):
-    def __init__(self, Ki, state_sv=0):
-        super().__init__(state_sv)
+    def __init__(self, Ki, activate=True, state_sv=0):
+        super().__init__(activate, state_sv)
         self.Ki = Ki
 
     def ode(self, t, x, u):
@@ -37,26 +41,65 @@ class I_Glied(Calculate):
         return dxdt
 
 
+class DT1_Glied():
+    def __init__(self, Tp, Td, activate=True):
+        self.Td = Td
+        self.Tp = Tp
+        self.pt1 = PT1_Glied(1, self.Tp)
+        self.x1 = 0
+        self.activate = activate
+
+    def calc(self, u, t, t2):
+        if self.activate:
+            x1 = self.pt1.calc(u, t, t + t2)
+            y = (-x1/self.Tp+u/self.Tp) * self.Td
+            return y
+        else:
+            return 0
+
+
 class D_Glied:
-    def __init__(self, Kd, u_init=None):
+    def __init__(self, Kd, activate=True, u_init=None):
         self.Kd = Kd
         self.u_past = u_init
+        self.activate = activate
 
     def calc(self, u, h):
-        if self.u_past is None:
+        if self.activate:
+            if self.u_past is None:
+                self.u_past = u
+            u_grad = [self.u_past, u]
+            du = np.gradient(u_grad, h)
             self.u_past = u
-        u_grad = [self.u_past, u]
-        du = np.gradient(u_grad, h)
-        self.u_past = u
-        return du[0] * self.Kd
+            return du[0] * self.Kd
+        else:
+            return 0
+
+
+class IT1_Glied(Calculate):
+    def __init__(self, Kp, Ti, activate=True, state_sv=0):
+        super().__init__(activate, state_sv)
+        self.Kp = Kp
+        self.Ti = Ti
+        self.P = P_Glied(self.Kp)
+        self.I = I_Glied(self.Kp / self.Ti)
+
+    def calc(self, u, t, t2):
+        y1 = self.P.calc(u)
+        y2 = self.I.calc(u, t, t2)
+        return y1+y2
 
 
 class P_Glied:
-    def __init__(self, Kp):
+    def __init__(self, Kp, activate=True):
         self.Kp = Kp
+        self.activate = activate
 
     def calc(self, u):
-        return self.Kp*u
+        if self.activate:
+            return self.Kp*u
+        else:
+            return 0
 
 
 class Simple_Calc:
@@ -95,3 +138,6 @@ class Inputfunction:
             return 1/2*(self.w1-self.w0)*np.tanh(2*np.pi/self.deltaT*(t-self.t0))+1/2*(self.w1+self.w0)
         if t > self.t0+self.deltaT:
             return self.w1
+
+    def zero(self):
+        return 0
--- Versuch3_1.py
+++ Versuch3_1.py
@@ -3,61 +3,182 @@ from numpy import cos, sin, tan
 import scipy.integrate as sci
 import matplotlib.pyplot as plt
 import Function_Blocks as FB
+import time
 
-# LISTING_START ParaClass
+# zu plottende Aufgabe, Arbeitspunkt und Strecke auswählen
+Aufgabennummer = 1
+A1 = True
+normaleStrecke = True
+
 
 class Parameters(object):
     pass
 
-# LISTING_START SimuPara
-# Simulation parameter
+# Simulationsparameter
 sim_para = Parameters()  # instance of class Parameters
 sim_para.t0 = 0.0          # start time
-sim_para.tf = 5.0     # final time
-sim_para.h = 0.001
+sim_para.tf = 20.0     # final time
+sim_para.h = 0.01
 
 sim_para.w0 = 0.0
 sim_para.t_step = 1.0
-sim_para.w1 = 10.0
+sim_para.w1 = 1.0
+sim_para.w2 = 0.0
+sim_para.z11 = 0.0
+sim_para.z21 = 0.0
 sim_para.deltaT = 0.001
 
-
-#step = Inputfunctions(sim_para.t_step, sim_para.w0, sim_para.w1, sim_para.t_step+sim_para.delta_T)
-input_step =FB.Inputfunction(sim_para.t_step, sim_para.w0, sim_para.w1, sim_para.deltaT)
 tt = np.arange(sim_para.t0, sim_para.tf + sim_para.h, sim_para.h)
-#u = step.jump_v(tt)
 
-pt1_block = FB.PT1_Glied(1, 1)
-I_block = FB.I_Glied(50)
-sub = FB.Simple_Calc()
-d_block = FB.D_Glied(1)
+#Erzeugen der Eingänge
+w1 =FB.Inputfunction(sim_para.t_step, sim_para.w0, sim_para.w1, sim_para.deltaT)
+w2 =FB.Inputfunction(sim_para.t_step, sim_para.w0, sim_para.w2, sim_para.deltaT)
+z11 = FB.Inputfunction(sim_para.t_step, sim_para.w0, sim_para.z11, sim_para.deltaT)
+z21 = FB.Inputfunction(sim_para.t_step, sim_para.w0, sim_para.z21, sim_para.deltaT)
 
-# simulation
-x_traj = []
-u_traj = []
-y_I = 0
-for x in range(len(tt)):
-    t = tt[x]
-    u_cur = input_step.linear(t)
-    u_traj.append(u_cur)
-
-    error = sub.sub(u_cur, y_I)
-    #y_d = d_block.calc(error, sim_para.h)
-    y_pt1 = pt1_block.calc(error, t, t + sim_para.h)
-    y_d = d_block.calc(y_pt1, sim_para.h)
-    x_traj.append(y_d)
+# Listen in denen Ergebnisse für das spätere Plotten gespeichert werden
+m1_traj=  []
+m2_traj = []
+x1_traj = []
+x2_traj = []
+w1_traj = []
+w2_traj = []
 
 
-fig1, (ax1, ax2) = plt.subplots(2)
-ax1.plot(tt, x_traj, '-o', color='blue', MarkerSize=2)
-ax2.plot(tt, u_traj)
+
+# Parameterfestlegung
+if A1:
+    Ti_p11 = 0.4
+    Ti_p12 = -0.8
+    Kp_p21 = -0.2
+    Ti_p21 = 1
+    Ti_p22 = 1.2
+else:
+    Ti_p11 = 0.4
+    Ti_p12 = -1.28
+    Kp_p21 = -0.32
+    Ti_p21 = 1
+    Ti_p22 = 1.2
+
+if normaleStrecke:
+    Kp_r11 = 1.699
+    Ti_r11 = 1.291
+    Kp_r22 = 0.566
+    Ti_r22 = 1.291
+else:
+    Kp_r11 = 2.972
+    Ti_r11 = 9.950
+    Kp_r22 = 0.991
+    Ti_r22 = 9.950
+
+"""
+Initialisierung der Blöcke
+Übergabeparameter bestehen aus folgenden Blockparametern: Zeit-/Verstärkungskonstanten und activation Flag
+activation Flag muss nicht übergeben werden (default ist True)
+um Block zu berechnen dessen .calc() Methode aufrufen: 
+calc_Parameter: Eingabe vom Block, aktueller Zeitpunkt, aktueller Zeitpunkt+1 (--> aktueller Zeitpunkt + Schrittweite)
+"""
+R11_block = FB.IT1_Glied(Kp_r11, Ti_r11)
+R22_block = FB.IT1_Glied(Kp_r22, Ti_r22)
+P11_block = FB.I_Glied(Ti_p11)
+P21_block = FB.PT1_Glied(Kp_p21, Ti_p21)
+P12_block = FB.I_Glied(Ti_p12)
+P22_block = FB.I_Glied(Ti_p22)
+calc = FB.Simple_Calc()
+
+x1 = 0.0
+x2 = 0.0
+h = sim_para.h
+
+# starte Timer zur Simulationszeitberechnung
+time1 = time.perf_counter()
+# Simulation
+if Aufgabennummer == 1:
+    for x in range(len(tt)):
+        t = tt[x]
+        w1_cur = w1.linear(t)
+        w2_cur = w2.zero()
+        w1_traj.append(w1_cur)
+        w2_traj.append(w2_cur)
+        z11_cur = z11.zero()
+        z21_cur = z21.zero()
+
+        e1 = calc.sub(w1_cur, x1)
+        e2 = calc.sub(w2_cur, x2)
+        m1 = R11_block.calc(e1, t, t+h)
+        m1_traj.append(m1)
+
+        m2 = R22_block.calc(e2, t, t+h)
+        m2_traj.append(m2)
+
+        u11 = calc.add(z11_cur, m1)
+        u21 = calc.add(z21_cur, m1)
+
+        y11 = P11_block.calc(u11, t, t+h)
+        y21 = P21_block.calc(u21, t, t+h)
+        u12 = m2
+        u22 = m2
+        y12 = P12_block.calc(u12, t, t+h)
+        y22 = P22_block.calc(u22, t, t+h)
+
+        x1 = calc.add(y11, y12)
+        x2 = calc.add(y22, y21)
+        x1_traj.append(x1)
+        x2_traj.append(x2)
+
+# weitere Aufgaben unter diese if Bedingungen
+elif Aufgabennummer == 2:
+    pass
+elif Aufgabennummer == 3:
+    pass
+
+
+# plot data
+print("Simulationszeit: " + str(time.perf_counter()-time1))
+fig1, (ax1_1, ax1_2) = plt.subplots(2)
+ax1_1.plot(tt, w1_traj, '-o', color='blue', MarkerSize=2)
+ax1_2.plot(tt, w2_traj)
 # axis label
-ax1.set(xlabel="Zeit", title="Sprungantwort")
-ax2.set(xlabel="Zeit", title="Eingangssignal")
-ax1.set_ylabel("", color='blue')
+ax1_1.set(xlabel="Zeit", title="Eingangssignal w1")
+ax1_2.set(xlabel="Zeit", title="Eingangssignal w2")
 # format x axis
 plt.tight_layout()
 # adding grid
-ax1.xaxis.grid(True)
-ax1.yaxis.grid(True)
+ax1_1.xaxis.grid(True)
+ax1_1.yaxis.grid(True)
+ax1_2.xaxis.grid(True)
+ax1_2.yaxis.grid(True)
+
+fig2, (ax2_1, ax2_2) = plt.subplots(2)
+ax2_1.plot(tt, m1_traj, '-o', color='blue', MarkerSize=2)
+ax2_2.plot(tt, m2_traj)
+# axis label
+ax2_1.set(xlabel="Zeit", title="m1")
+ax2_2.set(xlabel="Zeit", title="m2")
+
+# format x axis
+plt.tight_layout()
+# adding grid
+ax2_1.xaxis.grid(True)
+ax2_1.yaxis.grid(True)
+ax2_2.xaxis.grid(True)
+ax2_2.yaxis.grid(True)
+
+fig1, (ax3_1, ax3_2) = plt.subplots(2)
+ax3_1.plot(tt, x1_traj)
+ax3_2.plot(tt, x2_traj)
+# axis label
+ax3_1.set(xlabel="Zeit", title="Ausgang x1")
+ax3_2.set(xlabel="Zeit", title="Ausgang x2")
+# format x axis
+plt.tight_layout()
+# adding grid
+ax3_1.xaxis.grid(True)
+ax3_1.yaxis.grid(True)
+ax3_2.xaxis.grid(True)
+ax3_2.yaxis.grid(True)
+#plot anzeigen
 plt.show()
+
+
+
